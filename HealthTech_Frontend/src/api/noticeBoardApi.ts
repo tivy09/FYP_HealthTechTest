df71449b267cbe355e988dba811e4c4a0d877a56
tivy09 <67724597+tivy09@users.noreply.github.com>
2023-08-17 10:58:26 +0800

    Delete HealthTech_Frontend directory

--- HealthTech_Frontend/src/api/noticeBoardApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from "../utils/axios/AxiosHandlers"
-
-export const noticeBoardAPI = {
-  //appointment list
-  getAllNotice: async () => {
-    return await axios.get(`/admin/noticeBoards`)
-  },
-}
