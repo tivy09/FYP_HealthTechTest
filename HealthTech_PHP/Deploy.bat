c3777edbb05078335cda1f0064bc11291d188449
tivy09 <67724597+tivy09@users.noreply.github.com>
2023-08-17 10:59:23 +0800

    Delete HealthTech_PHP directory

--- HealthTech_PHP/Deploy.bat
+++
@@ -1,8 +0,0 @@
-start composer dump-autoload
-start php artisan migrate
-start php artisan db:seed --class=PermissionsTableSeeder
-start php artisan cache:clear
-start php artisan route:clear
-start php artisan config:clear 
-start php artisan view:clear
-exit
--- HealthTech_PHP/resources/lang/zh-Hans/pagination.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'previous' => '＆laquo; 以前',
-    'next'     => '下一个＆raquo;',
-];
